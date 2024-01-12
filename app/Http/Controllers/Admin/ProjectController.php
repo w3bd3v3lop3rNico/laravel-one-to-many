e48282d5fe297d95964cb5c0b7ba399f3bb627d8
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-12 17:59:41 +0100

    Fix edit & update controller functions

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -71,7 +71,9 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        return view('admin.projects.edit', compact('project'));
+        $types = Type::orderBy('name', 'ASC')->get();
+
+        return view('admin.projects.edit', compact('project', 'types'));
     }
 
     /**
@@ -82,7 +84,7 @@ class ProjectController extends Controller
         $request->validate([
             'title' => ['required', 'max:255', 'string', Rule::unique('projects')->ignore($project->id)],
             'description' => 'nullable|min:5|string',
-            // 'category_id' => 'nullable|exists:categories,id'
+            'type_id' => 'nullable|exists:types,id'
         ]);
         $data = $request->all();
 
