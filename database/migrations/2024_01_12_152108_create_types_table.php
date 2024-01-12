f8dbe448317b82fc0789fd76fb8259b1234cc9f0
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-12 16:39:22 +0100

    Add Types Table Migration

--- database/migrations/2024_01_12_152108_create_types_table.php
+++ database/migrations/2024_01_12_152108_create_types_table.php
@@ -9,10 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
@@ -20,7 +22,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('types');
     }
