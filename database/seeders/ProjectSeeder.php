d3ec0091007dc7008006ce3f62575b07f189ac59
Nicola Tabai <nicola.tabai98@gmail.com>
2024-01-12 17:16:28 +0100

    Fix Project Seeder with type random ids ('pluck' method)

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
@@ -15,12 +16,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $types = Type::all();
+        $typeIds = $types->pluck('id');
+
         for ($i = 0; $i < 10; $i++) {
             $new_project = new Project();
 
             $new_project->title = $faker->sentence(5);
             $new_project->description = $faker->text(100);
             $new_project->slug = Str::slug($new_project->title, '-');
+            $new_project->type_id = $faker->optional()->randomElements($typeIds);
             $new_project->save();
         
         }
